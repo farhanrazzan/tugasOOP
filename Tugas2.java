bec020a4c5c4f754402658df8cf305c060ab1756
farhanrazzan <103124980+farhanrazzan@users.noreply.github.com>
2022-04-06 21:21:10 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tugas2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas2 {
+
+    public static void main(String[] args) {
+        String firstName = "Farhan ";
+        String lastName = "Razzan";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
